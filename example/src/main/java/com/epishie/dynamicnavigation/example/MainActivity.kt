75ae6247277e120e87dc54eed3a8910653be19e4
epishie <sephie.iturralde@gmail.com>
2019-07-11 16:07:58 +0900

    Initial commit

---
+++ example/src/main/java/com/epishie/dynamicnavigation/example/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.epishie.dynamicnavigation.example
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.epishie.dynamicnavigation.example.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+}
